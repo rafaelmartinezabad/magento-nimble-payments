ba5735c5c4daf7614851bff451177d3dacf9634d
asanchez <asanchez@opensistemas.com>
2016-09-27 08:58:40 +0200

    change url to create gateway, now depend of config environment

--- app/code/community/Bbva/NimblePayments/Block/Adminhtml/System/Config/Fieldset/Payment.php
+++ app/code/community/Bbva/NimblePayments/Block/Adminhtml/System/Config/Fieldset/Payment.php
@@ -16,7 +16,8 @@ class Bbva_NimblePayments_Block_Adminhtml_System_Config_Fieldset_Payment
         $text_button = Mage::helper('core')->__('Signup now.');
         $text_button2 = Mage::helper('core')->__('Already registered.');
         $url_nimble = $this->get_gateway_url();
-        return "<div><img style='height:4.9rem;' src='{$url_logo}' alt='Nimble logo'/></div><p>{$message} <a class='button button-primary' href='https://www.nimblepayments.com/private/registration?utm_source=Magento_Settings&utm_medium=Referral%20Partners&utm_campaign=Creacion-Cuenta&partner=magento' target='_blank'>{$text_button}</a> "
+        $env = substr(split("://", $url_nimble)[1], 0, 3);
+        return "<div><img style='height:4.9rem;' src='{$url_logo}' alt='Nimble logo'/></div><p>{$message} <a class='button button-primary' href='https://$env.nimblepayments.com/private/registration?utm_source=Magento_Settings&utm_medium=Referral%20Partners&utm_campaign=Creacion-Cuenta&partner=magento' target='_blank'>{$text_button}</a> "
         . "&nbsp;&nbsp;<a style='cursor:pointer;' onclick=\"window.open('{$url_nimble}', '', 'width=800, height=578')\">{$text_button2}</a></p><hr />";
     }
         
