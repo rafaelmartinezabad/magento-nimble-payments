62b59bec635d412861a0ac952b3f433b682c09f9
asanchez <asanchez@opensistemas.com>
2016-10-11 18:00:50 +0200

    modify notification flow

--- app/code/community/Bbva/NimblePayments/Model/Notification.php
+++ app/code/community/Bbva/NimblePayments/Model/Notification.php
@@ -6,16 +6,13 @@ class Bbva_NimblePayments_Model_Notification extends Varien_object
     public function getMessage()
     {
         $message= array();
-        $checkout = Mage::getModel('nimblepayments/checkout');
         
-        if (!$checkout->is3leggedToken()) {
-            if($checkout->validCredentials()) {
-                $message['message'] = Mage::helper('core')->__('You are not authorized in Nimble Payments.');
-                $message['type'] = 'token';
-            } else {
-                $message['message'] = Mage::helper('core')->__('Activate plugin Nimble Payments by BBVA.');
-                $message['type'] = 'plugins';
-            }
+        if (!Mage::getStoreConfig('payment/nimblepayments_checkout/active')) {
+            $message['message'] = Mage::helper('core')->__('Activate plugin Nimble Payments by BBVA.');
+            $message['type'] = 'plugins';
+        } else if (!Mage::getModel('nimblepayments/checkout')->is3leggedToken()) {
+            $message['message'] = Mage::helper('core')->__('You are not authorized in Nimble Payments.');
+            $message['type'] = 'token';
         }
 
         return $message;
